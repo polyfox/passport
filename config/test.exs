e4d0e2cc9db57d776190725ff024f3172e9636ef
Corey Powell <mistdragon100@gmail.com>
2018-01-02 09:58:08 -0500

    Added support repo config

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,15 @@
 use Mix.Config
 
+config :passport, ecto_repos: [Passport.Support.Repo]
+config :passport, Passport.Support.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool_size: 10,
+  username: "postgres",
+  password: "postgres",
+  database: "passport_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
 config :passport,
   primary_repo: Passport.Support.Repo,
   replica_repo: Passport.Support.Repo
