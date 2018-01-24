13d04545928eaa8db5b7b043aa24c6b0bb01b2ed
Corey Powell <mistdragon100@gmail.com>
2018-01-23 19:36:52 -0500

    Small edits to the passport main module

--- lib/passport.ex
+++ lib/passport.ex
@@ -109,30 +109,30 @@ defmodule Passport do
   end
 
   @spec track_failed_attempts(term, remote_ip :: term) :: {:ok, term} | {:error, term}
-  def track_failed_attempts(record, remote_ip) do
-    record
+  def track_failed_attempts(entity, remote_ip) do
+    entity
     |> change()
     |> Lockable.track_failed_attempts(remote_ip)
     # because track_failed_attempts uses a prepare_changes, ecto believes the
-    # record has not changed and will not execute the update
+    # entity has not changed and will not execute the update
     # force: true, here ensures that the prepare_changes is ran
     |> Repo.primary().update(force: true)
   end
 
   @spec track_tfa_attempts(term, remote_ip :: term) :: {:ok, term} | {:error, term}
-  def track_tfa_attempts(record, remote_ip) do
-    record
+  def track_tfa_attempts(entity, remote_ip) do
+    entity
     |> change()
     |> TwoFactorAuth.track_tfa_attempts(remote_ip)
     # because track_failed_attempts uses a prepare_changes, ecto believes the
-    # record has not changed and will not execute the update,
+    # entity has not changed and will not execute the update,
     # force: true, here ensures that the prepare_changes is ran
     |> Repo.primary().update(force: true)
   end
 
   @spec confirm_email(Ecto.Changeset.t | term) :: {:ok, term} | {:error, term}
-  def confirm_email(record) do
-    record
+  def confirm_email(entity) do
+    entity
     |> change()
     |> Confirmable.confirm()
     |> Repo.primary().update()
@@ -142,42 +142,61 @@ defmodule Passport do
   Changeset used for password resets/changes
 
   Args:
-  * `record` - the user to set password for
+  * `entity` - the user to set password for
   * `params` - the parameters
   """
-  def changeset(record, params, :password) do
-    record
-    |> Authenticatable.changeset(params, :reset)
-    |> Recoverable.clear_reset_password()
+  def changeset(entity, params, :password) do
+    changeset = entity
+    changeset = if Config.features?(entity, :authenticatable) do
+      Authenticatable.changeset(changeset, params, :reset)
+    else
+      changeset
+    end
+    changeset = if Config.features?(entity, :recoverable) do
+      Recoverable.clear_reset_password(changeset)
+    else
+      changeset
+    end
   end
 
   @doc """
   Changeset used for updates
 
   Args:
-  * `record` - the user to update
+  * `entity` - the user to update
   * `params` - the parameters
   """
-  def changeset(record, params) do
-    record
-    |> TwoFactorAuth.changeset(params)
-    |> Authenticatable.changeset(params)
+  def changeset(entity, params) do
+    changeset = entity
+    changeset = if Config.features?(entity, :two_factor_auth) do
+      TwoFactorAuth.changeset(changeset, params)
+    else
+      changeset
+    end
+    if Config.features?(entity, :authenticatable) do
+      Authenticatable.changeset(changeset, params)
+    else
+      changeset
+    end
   end
 
   @spec prepare_reset_password(Ecto.Changeset.t | Recoverable.t) :: {:ok, term} | {:error, term}
-  def prepare_reset_password(record) do
-    record
-    |> change()
-    |> Recoverable.prepare_reset_password()
-    |> Repo.primary().update()
+  def prepare_reset_password(entity) do
+    changeset = change(entity)
+    changeset = if Config.features?(entity, :recoverable) do
+      Recoverable.prepare_reset_password(changeset)
+    else
+      changeset
+    end
+    Repo.primary().update(changeset)
   end
 
   @doc """
   Resets a user's password and clears any reset information
   """
   @spec reset_password(term, params) :: {:ok, term} | {:error, term}
-  def reset_password(record, params) do
-    record
+  def reset_password(entity, params) do
+    entity
     |> changeset(params, :password)
     |> Repo.primary().update()
   end
@@ -186,62 +205,90 @@ defmodule Passport do
   Clears a reset password request
 
   Args:
-  * `record` - the user to clear
+  * `entity` - the user to clear
   """
   @spec clear_reset_password(term) :: {:ok, term} | {:error, term}
-  def clear_reset_password(record) do
-    record
-    |> change()
-    |> Recoverable.clear_reset_password()
-    |> Repo.primary().update()
+  def clear_reset_password(entity) do
+    changeset = change(entity)
+    changeset = if Config.features?(entity, :recoverable) do
+      Recoverable.clear_reset_password(changeset)
+    else
+      changeset
+    end
+    Repo.primary().update(changeset)
   end
 
   @spec on_successful_sign_in(term, remote_ip :: term) :: {:ok, term} | {:error, term}
-  def on_successful_sign_in(record, remote_ip) do
-    record
-    |> change()
-    # log sign in
-    |> Trackable.track_sign_in(remote_ip)
-    # clear failed_attempts
-    |> Lockable.clear_failed_attempts()
-    # clear tfa attempts
-    |> TwoFactorAuth.clear_tfa_attempts()
-    |> Repo.primary().update()
+  def on_successful_sign_in(entity, remote_ip) do
+    changeset = change(entity)
+    changeset = if Config.features?(entity, :trackable) do
+      # log sign in
+      Trackable.track_sign_in(changeset, remote_ip)
+    else
+      changeset
+    end
+    changeset = if Config.features?(entity, :lockable) do
+      # clear failed_attempts
+      Lockable.clear_failed_attempts(changeset)
+    else
+      changeset
+    end
+    changeset = if Config.features?(entity, :two_factor_auth) do
+      # clear tfa attempts
+      TwoFactorAuth.clear_tfa_attempts(changeset)
+    else
+      changeset
+    end
+    Repo.primary().update(changeset)
   end
 
-  def entity_activated?(record) do
-    if Config.features?(record, :activatable) do
-      record.active
+  @doc """
+  Clears all failed login attempts for the entity.
+  """
+  def unlock_entity(entity) do
+    changeset = change(entity)
+    changeset = if Config.features?(entity, :lockable) do
+      # clear failed_attempts
+      Lockable.clear_failed_attempts(changeset)
+    else
+      changeset
+    end
+    Repo.primary().update(changeset)
+  end
+
+  def entity_activated?(entity) do
+    if Config.features?(entity, :activatable) do
+      entity.active
     else
       true
     end
   end
 
-  def entity_confirmed?(record) do
-    if Config.features?(record, :confirmable) do
-      !!record.confirmed_at
+  def entity_confirmed?(entity) do
+    if Config.features?(entity, :confirmable) do
+      !!entity.confirmed_at
     else
       true
     end
   end
 
-  def entity_locked?(record) do
-    if Config.features?(record, :lockable) do
-      !!record.locked_at
+  def entity_locked?(entity) do
+    if Config.features?(entity, :lockable) do
+      !!entity.locked_at
     else
       false
     end
   end
 
   @spec check_authenticatable(term, String.t) :: {:ok, term} | {:error, term}
-  def check_authenticatable(record, password) do
-    case Authenticatable.check_password(record, password) do
-      {:ok, record} ->
+  def check_authenticatable(entity, password) do
+    case Authenticatable.check_password(entity, password) do
+      {:ok, entity} ->
         cond do
-          not entity_activated?(record) -> {:error, :inactive}
-          not entity_confirmed?(record) -> {:error, :unconfirmed}
-          entity_locked?(record) -> {:error, :locked}
-          true -> {:ok, record}
+          not entity_activated?(entity) -> {:error, :inactive}
+          not entity_confirmed?(entity) -> {:error, :unconfirmed}
+          entity_locked?(entity) -> {:error, :locked}
+          true -> {:ok, entity}
         end
 
       {:error, _} = err -> err
