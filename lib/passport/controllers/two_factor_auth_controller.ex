0da2052c1019f7bf45ca09766c79c023b0774710
Corey Powell <mistdragon100@gmail.com>
2018-01-29 17:44:48 -0500

    Moved controller implementation out of quote block

--- lib/passport/controllers/two_factor_auth_controller.ex
+++ lib/passport/controllers/two_factor_auth_controller.ex
@@ -14,86 +14,35 @@ defmodule Passport.TwoFactorAuthController do
       POST /account/confirm/tfa
       """
       def create(conn, params) do
-        case Passport.Sessions.authenticate_entity(params["email"], params["password"]) do
-          {:ok, %{tfa_enabled: true} = entity} ->
-            Passport.APIHelper.send_unauthorized(conn, reason: "TFA already enabled")
-
-          {:ok, entity} ->
-            case Passport.prepare_tfa_confirmation(entity) do
-              {:ok, entity} ->
-                conn
-                |> put_status(201)
-                |> render("show.json", data: entity)
-
-              {:error, %Ecto.Changeset{} = changeset} ->
-                Passport.APIHelper.send_changeset_error(conn, changeset: changeset)
-            end
-
-          {:error, {:unauthorized, _entity}} ->
-            Passport.APIHelper.send_unauthorized(conn, reason: "Invalid email or password.")
-
-          {:error, :unauthorized} ->
-            # unauthorized, but no user
-            Passport.APIHelper.send_unauthorized(conn, reason: "Invalid email or password.")
-
-          {:error, {:missing, attr}} ->
-            Passport.APIHelper.send_parameter_missing(conn, fields: [attr])
-
-          {:error, :locked} ->
-            Passport.APIHelper.send_locked(conn, reason: "Too many failed attempts.")
-
-          {:error, _} ->
-            Passport.APIHelper.send_forbidden(conn)
-        end
+        Passport.TwoFactorAuthController.create(__MODULE__, conn, params)
       end
 
       @doc """
       GET /account/confirm/tfa/:token
       """
       def show(conn, params) do
-        case Passport.find_by_tfa_confirmation_token(two_factor_auth_model(), params["token"]) do
-          nil -> Passport.APIHelper.send_not_found(conn)
-          entity -> render conn, "show.json", data: entity
-        end
+        Passport.TwoFactorAuthController.show(__MODULE__, conn, params)
       end
 
       @doc """
       POST /account/confirm/tfa/:token
       """
       def confirm(conn, params) do
-        case Passport.find_by_tfa_confirmation_token(two_factor_auth_model(), params["token"]) do
-          nil -> Passport.APIHelper.send_not_found(conn)
-          entity ->
-            case Passport.TwoFactorAuth.abs_check_totp(entity, params["otp"]) do
-              false ->
-                Passport.APIHelper.send_unauthorized(conn, reason: "otp mismatch")
-              true ->
-                case Passport.confirm_tfa(entity) do
-                  {:ok, entity} ->
-                    Passport.APIHelper.send_no_content(conn)
-                  {:error, %Ecto.Changeset{} = changeset} ->
-                    Passport.APIHelper.send_changeset_error(conn, changeset: changeset)
-                end
-            end
-        end
+        Passport.TwoFactorAuthController.confirm(__MODULE__, conn, params)
       end
 
       @doc """
       DELETE /account/confirm/tfa/:token
       """
       def delete(conn, params) do
-        case Passport.find_by_tfa_confirmation_token(two_factor_auth_model(), params["token"]) do
-          nil -> Passport.APIHelper.send_not_found(conn)
-          entity ->
-            case Passport.cancel_tfa_confirmation(entity) do
-              {:ok, _entity} -> Passport.APIHelper.send_no_content(conn)
-              {:error, %Ecto.Changeset{} = changeset} ->
-                Passport.APIHelper.send_changeset_error(conn, changeset: changeset)
-            end
-        end
+        Passport.TwoFactorAuthController.delete(__MODULE__, conn, params)
       end
     end
   end
+
+  import Plug.Conn
+  import Phoenix.Controller
+  import Passport.APIHelper
 
   @doc """
   two_factor_auth_model denotes what module represents the TwoFactorAuth entity.
@@ -101,4 +50,75 @@ defmodule Passport.TwoFactorAuthController do
   This function should return a valid Ecto.Schema model
   """
   @callback two_factor_auth_model() :: atom
+
+  def create(_controller, conn, params) do
+    case Passport.Sessions.authenticate_entity(params["email"], params["password"]) do
+      {:ok, %{tfa_enabled: true} = entity} ->
+        send_unauthorized(conn, reason: "TFA already enabled")
+
+      {:ok, entity} ->
+        case Passport.prepare_tfa_confirmation(entity) do
+          {:ok, entity} ->
+            conn
+            |> put_status(201)
+            |> render("show.json", data: entity)
+
+          {:error, %Ecto.Changeset{} = changeset} ->
+            send_changeset_error(conn, changeset: changeset)
+        end
+
+      {:error, {:unauthorized, _entity}} ->
+        send_unauthorized(conn, reason: "Invalid email or password.")
+
+      {:error, :unauthorized} ->
+        # unauthorized, but no user
+        send_unauthorized(conn, reason: "Invalid email or password.")
+
+      {:error, {:missing, attr}} ->
+        send_parameter_missing(conn, fields: [attr])
+
+      {:error, :locked} ->
+        send_locked(conn, reason: "Too many failed attempts.")
+
+      {:error, _} ->
+        send_forbidden(conn)
+    end
+  end
+
+  def show(controller, conn, _params) do
+    case Passport.find_by_tfa_confirmation_token(controller.two_factor_auth_model(), conn.path_params["token"]) do
+      nil -> send_not_found(conn)
+      entity -> render conn, "show.json", data: entity
+    end
+  end
+
+  def confirm(controller, conn, params) do
+    case Passport.find_by_tfa_confirmation_token(controller.two_factor_auth_model(), conn.path_params["token"]) do
+      nil -> Passport.APIHelper.send_not_found(conn)
+      entity ->
+        case Passport.TwoFactorAuth.abs_check_totp(entity, params["otp"]) do
+          false ->
+            Passport.APIHelper.send_unauthorized(conn, reason: "otp mismatch")
+          true ->
+            case Passport.confirm_tfa(entity) do
+              {:ok, entity} ->
+                Passport.APIHelper.send_no_content(conn)
+              {:error, %Ecto.Changeset{} = changeset} ->
+                Passport.APIHelper.send_changeset_error(conn, changeset: changeset)
+            end
+        end
+    end
+  end
+
+  def delete(controller, conn, params) do
+    case Passport.find_by_tfa_confirmation_token(controller.two_factor_auth_model(), conn.path_params["token"]) do
+      nil -> send_not_found(conn)
+      entity ->
+        case Passport.cancel_tfa_confirmation(entity) do
+          {:ok, _entity} -> send_no_content(conn)
+          {:error, %Ecto.Changeset{} = changeset} ->
+            send_changeset_error(conn, changeset: changeset)
+        end
+    end
+  end
 end
